07d59d2e06e83155bc4ccd6cc5ab4a597cde55c3
Damien Goehrig <damien.goehrig@gmail.com>
2021-11-19 18:03:40 -0500

    Working Auth

--- routes/auth.js
+++ routes/auth.js
@@ -9,19 +9,19 @@ const User = require('../models/user')
 
 router.post('/login', async (req, res) => {
   
-  const user = User.find({"User.email": req.body.email})
-  console.log(user.password)
-  console.log(req.body.email)
-  if (null === user) {
-    return res.status(400).json({message: 'User not found'})
-  }
-
   try {
+    const user = await User.findOne({ email: req.body.email})
+        
+    if (null === user) {
+      return res.status(400).json({message: 'User not found'})
+    }
+
     if (await bcrypt.compare(req.body.password, user.password)) {
       res.status(200).json({message: 'Success'})
     } else {
       res.status(400).json({message: 'Invalid password'})
     }
+
   } catch (err) {
     res.status(500).json({message: err.message})
   }
